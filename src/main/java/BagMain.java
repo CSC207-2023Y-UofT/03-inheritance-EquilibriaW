342e18583190febb7d6d3cafbb750e131b4ba2cb
alex <Alex.wang1903@gmail.com>
2023-06-11 19:17:55 -0400

    Commit

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -7,7 +7,6 @@
  */
 class BagMain {
     /**
-     * TODO: Implement this method
      * Enhance each of the bags in bags. If double_enhance_handbags is
      * True, then enhance any HandBags a second time.
      *
@@ -15,11 +14,17 @@ class BagMain {
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
+        for(int i = 0; i < bags.length; i++){
+            if (bags[i] instanceof HandBag & double_enhance_handbags){
+                bags[i].enhance();
+                bags[i].enhance();
+            } else {
+                bags[i].enhance();
+            }
+        }
     }
 
     /**
-     * TODO: Implement this method
      * Return the total number of straps in CrossbodyBags in bags.
      * Any bag that is not a CrossbodyBag should not be included
      * in the count!
@@ -28,6 +33,12 @@ class BagMain {
      * @return The total number of straps of CrossbodyBags.
      */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
+        int rtotal = 0;
+        for(int i = 0; i < bags.length; i++){
+            if (bags[i] instanceof CrossbodyBag){
+                rtotal += bags[i].getNumberOfContents();
+            }
+        }
+        return rtotal;
     }
 }
